e638c44aefa5e0464497d77a8fb7d900b8fa7c07
MD MASUM <mdmasumdu45@gmail.com>
2023-09-30 04:28:30 +0600

    created a reg and login page with firebase

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,74 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef, useState } from "react";
+import auth from "../../Firebase.config";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+  const [succes,setSucces] =useState('');
+  const [errora,setErrora] = useState('')
+
+
+const emailref = useRef()
+  // forgot password email sent
+  const forgothandle =()=>{
+
+    setErrora('')
+    const email =emailref.current.value;
+    if(!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)){
+      setErrora('provide your email correctly')
+    }
+    sendPasswordResetEmail(auth,email)
+    .then(()=>{
+      setSucces('')
+      setSucces("An password reset email has been sent")
+    })
+    .catch((error)=>{console.log(error)})
+
+
+
+
+  }
+
+  const loginhandler =(e)=>{
+    e.preventDefault()
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+  
+
+    setErrora('')
+    setSucces('')
+
+    if(password.length<6){
+      setErrora('Password should at least 6 character')
+      return;
+  }
+  else if(!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)){
+      setErrora("Provide your email correctly")
+      return;
+  }
+
+  else if(!/[A-Z]/.test(password)){
+      setErrora('Your password must Contains at least one uppercase letter')
+
+      return;
+  }
+
+
+    signInWithEmailAndPassword(auth,email,password)
+    .then(result=>{
+      if(result.user.emailVerified){
+        console.log(result.user)
+        setSucces('succesfully signed in')
+      }
+      else{
+        setErrora('Please verify your email')
+      }
+      
+    })
+    .catch(error=> setErrora(error.message))
+
+  }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -10,24 +79,32 @@ const Login = () => {
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
+      {
+                    errora ? <p className="text-red-700">{errora}</p> : ''
+                }
+
+                {
+                    succes ? <p className="text-green-700">{succes}</p> :''
+                    
+                }
+        <form onSubmit={loginhandler}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="email" placeholder="email" ref={emailref} name="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="password" placeholder="password" name="password" className="input input-bordered" />
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <Link onClick={forgothandle}>Forgot password?</Link>
           </label>
         </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
+       <input type="submit" value="Submit" className="btn btn-primary" />
+        </form>
       </div>
     </div>
   </div>
--- src/Components/Registration/Registration.jsx
+++ src/Components/Registration/Registration.jsx
@@ -1,12 +1,14 @@
-import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import auth from "../../Firebase.config";
+import { FaEye,FaEyeSlash} from 'react-icons/fa';
 
 
 const Registration = () => {
 
     const [succes,setSucces] =useState('');
     const [errora,setErrora] = useState('')
+    const [show,setShow] = useState(false)
  
 
 
@@ -14,11 +16,12 @@ const Registration = () => {
         e.preventDefault()
         const email = e.target.email.value;
         const password= e.target.password.value;
+        const name= e.target.text.value;
 
         setErrora('')
         setSucces('')
 
-        console.log(email,password)
+        console.log(email,password,name)
         if(password.length<6){
             setErrora('Password should at least 6 character')
             return;
@@ -37,7 +40,14 @@ const Registration = () => {
         createUserWithEmailAndPassword(auth,email,password)
         .then(result=>{
             
-            console.log(result)
+
+            // update profile
+            updateProfile(result.user,{
+                displayName:name,
+                photoURL: "https://example.com/jane-q-user/profile.jpg"
+            })
+
+            // send email verification
             sendEmailVerification(result.user)
             .then(()=> alert('Email verification sent'))
             .catch((error)=> console.log(error))
@@ -70,7 +80,12 @@ const Registration = () => {
             <form onSubmit={regHandler}>
                 <input type="email" name="email" id="" placeholder="Your Email" className=" p-2 md:w-96" required />
                 <br />
-                <input type="password" name="password" id="" placeholder="Password" className="my-5 p-2 md:w-96" required />
+                <input type="text" name="text" id="" placeholder="Your Name" className=" p-2 md:w-96 mt-5" required />
+                <br />
+                <div>
+                <input type={show ? "password": 'text'} name="password" id="" placeholder="Password" className="my-5 p-2 md:w-96 relative" required />
+                <span className="absolute mt-8 -ml-6" onClick={()=>setShow(!show)}> {show ?<FaEye></FaEye>:<FaEyeSlash></FaEyeSlash> }</span>
+                </div>
                 <br />
                 <input type="submit" value="Submit"  className=" btn btn-secondary p-2 md:w-96"/>
 
